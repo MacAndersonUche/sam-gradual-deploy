3109dae1c059776103a417e6d375c35963bab704
MacAnderson Uche <macandersonuche@yahoo.com>
2024-03-20 21:15:40 +0000

    feat: base config

---
+++ src/handlers/index.ts
@@ -0,0 +1,34 @@
+import { APIGatewayEvent } from 'aws-lambda';
+
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { PutCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+const main = async (id: string) => {
+  const command = new PutCommand({
+    TableName: 'traced-table',
+    Item: {
+      id,
+    },
+  });
+
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+};
+
+export const handler = async (event: APIGatewayEvent) => {
+  try {
+    const { id }: { id: string } = JSON.parse(event.body!);
+
+    await main(id);
+
+    return {
+      status: 200,
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
