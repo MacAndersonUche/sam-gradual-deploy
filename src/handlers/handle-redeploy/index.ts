86014cc33451f3aefda8f3035c9e78de749e3ac7
MacAnderson Uche <macandersonuche@yahoo.com>
2024-03-24 15:21:08 +0000

    feat:modify cloudformation

--- src/handlers/handle-redeploy/index.ts
+++ src/handlers/handle-redeploy/index.ts
@@ -1,16 +1,19 @@
-import { SNSEvent } from 'aws-lambda';
+import { SNSEvent, SQSEvent } from 'aws-lambda';
 import { handleGithub } from './github-helpers';
 import { createBuildClient } from './codebuild-helpers';
 require('dotenv').config();
 
-export const handler = async (event: SNSEvent) => {
+export const handler = async (event: SQSEvent) => {
   try {
     //TO DO: GET PROJECT NAME FROM EVENT
     //1st step get the project name from SNS
-    console.log('SNS MESSAGE', JSON.parse(event.Records[0].Sns.Message));
+    console.log('SQS MESSAGE', JSON.parse(event.Records[0].body));
+
+    // await handleGithub('sam-gradual-deploy');
+    //5th step using the project name and the aws-sdk create a build for codebuild
 
     await handleGithub('sam-gradual-deploy');
-    //5th step using the project name and the aws-sdk create a build for codebuild
+
     await createBuildClient('sam-gradual-deploy');
 
     return {
