e71c04057c72d68b924f90030a5f6ecec07611fc
MacAnderson Uche <macandersonuche@yahoo.com>
2024-03-24 22:01:31 +0000

    fix:pass test

--- src/__tests__/handlers.test.ts
+++ src/__tests__/handlers.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 describe('Test', () => {
   it('Should test', () => {
-    expect(1 + 1).toEqual(3);
+    expect(1 + 1).toEqual(2);
   });
 });
