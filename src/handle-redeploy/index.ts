e484f89dec5c34b010435a36bd6cb0efc4b18d6a
MacAnderson Uche <macandersonuche@yahoo.com>
2024-03-24 20:26:27 +0000

    feat: add project name

--- src/handle-redeploy/index.ts
+++ src/handle-redeploy/index.ts
@@ -1,13 +1,20 @@
 import { SNSEvent, SQSEvent } from 'aws-lambda';
 import { handleGithub } from './github-helpers';
 import { createBuildClient } from './codebuild-helpers';
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 
 export const handler = async (event: SQSEvent) => {
   try {
-    //TO DO: GET PROJECT NAME FROM EVENT
-    //1st step get the project name from SNS
-    console.log('SQS MESSAGE', JSON.parse(event.Records[0].body));
+    console.log(
+      'SQS EVENT FROM EVENT BRIDGE',
+      JSON.parse(event.Records[0].body)
+    );
+
+    const body = JSON.parse(event.Records[0].body);
+
+    const projectName = body.detail['project-name'] as string;
+
+    console.log({ projectName });
     //5th step using the project name and the aws-sdk create a build for codebuild
 
     // await handleGithub('sam-gradual-deploy');
