551b5556fd10c7e241b3d490509c8d5919aaed67
MacAnderson Uche <macandersonuche@yahoo.com>
2024-03-24 06:18:50 +0000

    feat:restructure

--- src/handlers/handle-redeploy/github-helpers.ts
+++ src/handlers/handle-redeploy/github-helpers.ts
@@ -8,8 +8,7 @@ async function graphqlClient(graphqlQuery: string) {
     body: graphqlQuery,
   });
 
-  const responseData = await response.json();
-  if (responseData.ok) {
+  if (response.ok) {
     const data = await response.json();
     return data;
   }
@@ -40,10 +39,10 @@ async function revertLastMergedPullRequestByID(id: string) {
   const graphqlQuery = JSON.stringify({
     query: `mutation {
       revertPullRequest(input: {
-        pullRequestId: ${id},
+        pullRequestId: "${id}",
       }) {
-      revertPullRequest {
-			node_id
+      pullRequest {
+			id
 		}
       }
     }`,
@@ -67,6 +66,9 @@ async function mergeRevertedPullRequestByID(id: string) {
   });
 
   const responseData = await graphqlClient(graphqlQuery);
+
+  console.log({ responseData });
+
   return responseData;
 }
 
