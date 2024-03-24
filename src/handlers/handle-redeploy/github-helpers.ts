dbe762d792c98cc3c0a9eef454e50d1fcdf78cd7
MacAnderson Uche <90990516+MacAndersonUche@users.noreply.github.com>
2024-03-24 07:01:05 +0000

    Revert "feat:log1"

--- src/handlers/handle-redeploy/github-helpers.ts
+++ src/handlers/handle-redeploy/github-helpers.ts
@@ -88,15 +88,9 @@ export async function handleGithub(repoName: string) {
 
   console.log({ revertedPullRequestID: JSON.stringify(lastMergedPRID) });
 
-  const { data } = await mergeRevertedPullRequestByID(lastMergedPRID);
+  const merged = await mergeRevertedPullRequestByID(lastMergedPRID);
 
-  console.log({
-    merged: JSON.stringify(data),
-    mergedUnString: data,
-    mergedRe: data.mergePullRequest,
-    pullRe: data.mergePullRequest.pullRequest,
-    state: data.mergePullRequest.pullRequest.state,
-  });
+  console.log({ merged: JSON.stringify(merged) });
 
-  return data.mergePullRequest === 'MERGED' ? true : false;
+  return merged === 'MERGED' ? true : false;
 }
