43fffcfe37d7345be4cb9b5cd51453f789b432c0
MacAnderson Uche <macandersonuche@yahoo.com>
2024-03-24 19:09:37 +0000

    feat: remove redundant code

--- src/handlers/handle-redeploy/index.ts
+++ src/handlers/handle-redeploy/index.ts
@@ -8,23 +8,16 @@ export const handler = async (event: SQSEvent) => {
     //TO DO: GET PROJECT NAME FROM EVENT
     //1st step get the project name from SNS
     console.log('SQS MESSAGE', JSON.parse(event.Records[0].body));
-
-    // await handleGithub('sam-gradual-deploy');
     //5th step using the project name and the aws-sdk create a build for codebuild
 
-    await handleGithub('sam-gradual-deploy');
+    // await handleGithub('sam-gradual-deploy');
 
-    await createBuildClient('sam-gradual-deploy');
+    // await createBuildClient('sam-gradual-deploy');
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        hi: 'here',
-      }),
     };
   } catch (e) {
     console.log(e);
   }
 };
-
-handleGithub('sam-gradual-deploy').then((res) => console.log({ res }));
