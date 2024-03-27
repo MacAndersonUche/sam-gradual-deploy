67b222a3cc541130552c07bf3a766ebfd54439bd
MacAnderson Uche <macandersonuche@yahoo.com>
2024-03-27 23:24:30 +0000

    feat: try

--- checkGithubWorkflowStatus.js
+++ checkGithubWorkflowStatus.js
@@ -1,31 +1,51 @@
 const axios = require('axios');
-async function checkGithubWorkflowStatus(runId) {
-  const url = `https://api.github.com/repos/<owner>/<repo-b>/actions/runs/${runId}`;
+require('dotenv').config();
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function checkGithubWorkflowStatus() {
+  const url = `https://api.github.com/repos/TracedLtd/Control-E2E-Tests/actions/workflows/e2e-tests-pipeline.yml/runs`;
   const headers = {
-    Authorization: 'token <your-github-token>',
+    Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
     Accept: 'application/vnd.github.v3+json',
+    'X-GitHub-Api-Version': '2022-11-28',
   };
+
   try {
-    const response = await axios.get(url, { headers });
-    if (response.status === 200) {
-      const workflowStatus = response.data.status;
-      if (
-        workflowStatus === 'completed' &&
-        response.data.conclusion !== 'success'
-      ) {
-        console.log('GitHub Actions workflow failed.');
-        process.exit(1);
+    let response;
+    let hasBeenCompleted = false;
+    do {
+      response = await axios.get(url, { headers });
+
+      if (response.status === 200) {
+        const workflowStatus = response.data.workflow_runs[0];
+
+        console.log({ workflowStatus });
+        if (workflowStatus === 'completed') {
+          if (response.data.conclusion === 'success') {
+            console.log('GitHub Actions workflow succeeded.');
+            hasBeenCompleted = true;
+            return true;
+          } else {
+            console.log('GitHub Actions workflow failed.');
+            hasBeenCompleted = true;
+            return false;
+          }
+        } else {
+          // Wait for 30 seconds before checking again
+          await sleep(15000);
+        }
       } else {
-        console.log('GitHub Actions workflow succeeded.');
+        throw response;
       }
-    } else {
-      console.log('Failed to check workflow status.');
-      process.exit(1);
-    }
+    } while (hasBeenCompleted);
   } catch (error) {
     console.error('Error checking workflow status:', error);
-    process.exit(1);
+    return false;
   }
 }
-const runId = process.argv[3];
-checkGithubWorkflowStatus(runId);
+
+// Call the function to start checking GitHub workflow status
+checkGithubWorkflowStatus();
--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-    // if (response.status === 204) {
-    //   console.log('Workflow triggered successfully.');
-    //   // Assuming the response includes the run ID in the response body
-    //   const runId = response.data.id;
-    //   fs.writeFileSync('run_id.txt', runId);
-    // } else {
-    //   console.log('Failed to trigger workflow.');
-    //   process.exit(1);
-    // }
